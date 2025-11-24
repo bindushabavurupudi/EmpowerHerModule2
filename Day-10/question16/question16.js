a9e905a0e19278a6b9c08ab69df0bf7268f1a975
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2025-11-24 23:44:11 +0530

    added q15

---
+++ Day-10/question16/question16.js
@@ -0,0 +1,7 @@
+
+const isEven = n => n % 2 === 0
+console.log(isEven(5))
+const result = (marks) => (marks >= 35) ? "Pass" : "Fail";
+const greet = (name) => `Hello, ${name ? name : "Guest"}`;
+console.log(greet("Bindusha"))
+console.log(result(69))
