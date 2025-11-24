f4722a3ddaf31aef44c39d0539d3dce79f203b9b
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2025-11-24 23:02:34 +0530

    added q13

---
+++ Day-10/question13/question13.js
@@ -0,0 +1,8 @@
+const people = [ { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, { name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } ];
+
+const [{name: name1, address: {city:city1, street:{name:stName1}}}, {name:name2, address: {city:city2, street:{name:stName2}}}] = people;
+let arr = [];
+arr.push(`${name1} lives in ${city1} on ${stName1}`);
+arr.push(`${name2} lives in ${city2} on ${stName2}`);
+console.log(arr)
+
