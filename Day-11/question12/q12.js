d8a2d606b28c6ae1a48aeca7529b3396b48e8643
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2025-11-25 23:01:50 +0530

    added q12

---
+++ Day-11/question12/q12.js
@@ -0,0 +1,28 @@
+function processStudents(obj){
+    let toppers = obj.filter(x => {
+        return x.marks > 60
+    });
+    toppers.sort((x, y) =>{
+        return (y.marks-x.marks);
+    });
+    // console.log(toppers);
+    let students = toppers.map(x=>x.name);
+    console.log(students)
+}
+let arr =  [{ "name": "Alice", "marks": 58 },
+    { "name": "Bob", "marks": 85 },
+    { "name": "Charlie", "marks": 92 },
+  { "name": "David", "marks": 45 },
+  { "name": "Emma", "marks": 76 },
+  { "name": "Frank", "marks": 63 },
+  { "name": "Grace", "marks": 89 },
+  { "name": "Hannah", "marks": 95 },
+  { "name": "Ian", "marks": 54 },
+  { "name": "Jack", "marks": 79 },
+  { "name": "Kate", "marks": 67 },
+  { "name": "Leo", "marks": 88 },
+  { "name": "Mia", "marks": 91 },
+  { "name": "Nathan", "marks": 72 },
+  { "name": "Olivia", "marks": 82 }
+]
+processStudents(arr);
